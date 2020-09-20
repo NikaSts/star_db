37bfe728ec1d79609a8b396b6b4b71f0f64b3dc3
Nefila <veronika.sts@gmail.com>
2020-09-20 22:34:03 +0200

    Added hoc with-active-item

---
+++ src/hocs/with-active-item.js
@@ -0,0 +1,31 @@
+import React, { PureComponent } from 'react';
+
+const withActiveItem = (Component) => {
+  class Wrapper extends PureComponent {
+    constructor(props) {
+      super(props);
+      this.state = {
+        activeItem: undefined,
+      };
+      this.handleItemClick = this.handleItemClick.bind(this);
+    }
+
+    handleItemClick(activeItem) {
+      this.setState({ activeItem });
+    }
+
+    render() {
+      const { activeItem } = this.state;
+      return (
+        <Component
+          {...this.props}
+          activeItem={activeItem}
+          onItemClick={this.handleItemClick}
+      />
+      );
+    }
+  }
+  return Wrapper;
+};
+
+export default withActiveItem;
