9b9567a290c9f4a31fc75862252025ab5798042c
Nefila <veronika.sts@gmail.com>
2020-09-21 00:15:50 +0200

    Added hoc with-data

---
+++ src/hocs/with-data.js
@@ -0,0 +1,66 @@
+import React, { PureComponent } from 'react';
+import Spinner from '../components/spinner';
+import ErrorMessage from '../components/error-message';
+
+
+const withData = (Component, getData) => {
+  class Wrapper extends PureComponent {
+    constructor(props) {
+      super(props);
+      this.state = {
+        data: [],
+        isLoading: false,
+        hasError: false,
+      };
+
+      this.updateData = this.updateData.bind(this);
+      this.onDataLoaded = this.onDataLoaded.bind(this);
+      this.onError = this.onError.bind(this);
+    }
+
+    componentDidMount() {
+      this.updateData();
+    }
+
+    onError() {
+      this.setState({
+        isLoading: false,
+        hasError: true,
+      });
+    }
+
+    onDataLoaded(data) {
+      this.setState({
+        data,
+        isLoading: false,
+      });
+    }
+
+    updateData() {
+      this.setState({
+        isLoading: true,
+      });
+      getData()
+        .then(this.onDataLoaded)
+        .catch(this.onError);
+    }
+
+    render() {
+      const { data, isLoading, hasError } = this.state;
+
+      if (isLoading) {
+        return <Spinner />;
+      }
+
+      if (hasError) {
+        return <ErrorMessage />;
+      }
+
+      return <Component {...this.props} items={data} />;
+    }
+  }
+
+  return Wrapper;
+};
+
+export default withData;
